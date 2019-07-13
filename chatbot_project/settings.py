68a332724bd56109c13c1a7533cb2e498e1bcdf0
YasmineChtourou <yasmine.chtourou@enis.tn>
2019-07-13 11:57:40 +0200

    Third commit

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'chatbot_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sentiment_context',                      
+        'USER': 'postgres',
+        'PASSWORD': 'Equipe1pfe',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
